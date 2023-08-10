722e3cb8879b90d121c12f7d323fbf3c84d48560
saurabhdev7 <sdsilarin@gmail.com>
2023-08-11 02:11:11 +0530

    javascript essentials

--- hoisting.js
+++
@@ -1,9 +0,0 @@
-// Hoisting - It moves declaration to the top of the scope before execution.
-// It is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
-// Hoisting does not work with let and const variables and throws an error.
-
-hello();
-
-function hello() {
-    console.log("Hello World");
-}
