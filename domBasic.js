7cfb1e42d72c18d3495d26d43ad56e9c660ecdcb
saurabhdev7 <sdsilarin@gmail.com>
2023-08-23 00:59:18 +0530

    javascript essentials

--- domBasic.js
+++ domBasic.js
@@ -1,6 +1,14 @@
 // DOM Manipulation : Document Object Model
 
 
-// 1 - getElementById :
+// 1 - .getElementById :
 
 const heading = document.getElementById('heading');
+console.log(heading);
+
+
+
+// 2 - .getElementsByTagName : 
+
+const tags = document.getElementsByTagName('h');
+console.log(tags);
