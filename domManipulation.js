ec03ea40aa4f45c9549198715fa3cc7332ab334e
saurabhdev7 <sdsilarin@gmail.com>
2023-10-09 23:54:47 +0530

    javascript essentials

--- domManipulation.js
+++ domManipulation.js
@@ -1,3 +1,5 @@
+/*
+
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -6,3 +8,9 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
+
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+    console.log(document.querySelector('.guess').value);
+});
